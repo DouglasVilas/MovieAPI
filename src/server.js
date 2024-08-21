29e1ae3c34abf018765dbcbd8410740003fabf97
DouglasVilasBoas <douglas.silvabatista1013.dsb@gmail.com>
2024-08-20 21:38:01 -0300

    analisar todo o código

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ app.use((error, request, response, next) => {
 
 });
 
-const PORT = 3334; /
+const PORT = 3334;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); 
 
-//parei na aula Organizando a estrutura do projeto 
+// Reever o codigo todo
